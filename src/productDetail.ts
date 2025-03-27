722aeabc1a8762e9ce0f0fd26ccd1639fcacc542
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-03-27 13:18:52 +0530

    fix: controller object creation only after DOM content load, prevent default behaviour of anchor on cart button

--- src/productDetail.ts
+++ src/productDetail.ts
@@ -1,5 +1,6 @@
 import { ProductDetailController } from "./controller/productDetailsController";
 import { ProductDetailView } from "./view/productDetailsView";
 
-const productDetailcontroller = new ProductDetailController(new ProductDetailView());
-document.addEventListener("DOMContentLoaded",() => productDetailcontroller);
+document.addEventListener("DOMContentLoaded", () => {
+  new ProductDetailController(new ProductDetailView());
+});
